acd0b6f4e59e9b3d22da22decb47bfe633dee260
Jakob Jul Elben <jelben@deloitte.dk>
2021-04-16 16:46:06 +0200

    Add wait option

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,6 +7,6 @@ bash scripts/build.sh
 # Setup k8s cluster
 kind create cluster
 
-kubectl apply -f k8s/deployment.yml
+kubectl apply --wait -f k8s/deployment.yml 
 
 kubectl exec -it deploy/fastapi-deployment -- curl http://localhost:80

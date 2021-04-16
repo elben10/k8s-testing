980d6cb5a4526d1a827eb85a127ee239d88b1bca
Jakob Jul Elben <jelben@deloitte.dk>
2021-04-16 16:50:07 +0200

    wait for pods

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,6 +7,8 @@ bash scripts/build.sh
 # Setup k8s cluster
 kind create cluster
 
-kubectl apply --wait -f k8s/deployment.yml 
+kubectl apply -f k8s/deployment.yml
+
+kubectl wait --for=condition=available --timeout=60s --all deployments
 
 kubectl exec -it deploy/fastapi-deployment -- curl http://localhost:80

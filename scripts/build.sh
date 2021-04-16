19e4b585d11b864e769042bc0a4d774d89565572
Jakob Jul Elben <jelben@deloitte.dk>
2021-04-16 15:42:50 +0200

    Refactor

---
+++ scripts/build.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+set -e
+
+docker buildx bake \
+    --set app.cache-from=type=registry,ref=ghcr.io/${REPOSITORY?Variable not set}/dummy:latest \
+    --set app.cache-to=type=inline \
+    --push

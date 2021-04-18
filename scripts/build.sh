503193f01400bfde333860069054d253ac8d9418
Jakob Jul Elben <elbenjakobjul@gmail.com>
2021-04-18 11:39:52 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,5 @@ set -e
 
 docker buildx bake \
     --set app.cache-from=type=registry,ref=ghcr.io/${REPOSITORY?Variable not set}/dummy:latest \
-    --set app.cache-to=type=inline 
+    --set app.cache-to=type=inline \
+    --pull

d60149b6a89b3a9031bc8c7880fd6c2aed97f068
Jakob Jul Elben <jelben@deloitte.dk>
2021-04-18 12:15:09 +0200

    ....

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,10 +4,10 @@ set -e
 
 bash scripts/build.sh
 
+kind load docker-image ghcr.io/elben10/dummy:latest
+
 kubectl apply -f k8s/deployment.yml
 
 kubectl wait --for=condition=available --timeout=60s --all deployments
 
-kubectl describe pod $(kubectl get pod -l app=fastapi -o jsonpath="{.items[0].metadata.name}")
-
-kubectl exec -it deploy/fastapi-deployment -- bash curl http://localhost
+kubectl exec -it deploy/fastapi-deployment -- bash  -c 'curl http://localhost'

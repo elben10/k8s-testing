d3f9348e3e8494f95731f40672b625efe46a3947
Jakob Jul Elben <jelben@deloitte.dk>
2021-04-18 12:09:34 +0200

    Check if it works

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,3 +9,5 @@ kubectl apply -f k8s/deployment.yml
 kubectl wait --for=condition=available --timeout=60s --all deployments
 
 kubectl describe pod $(kubectl get pod -l app=fastapi -o jsonpath="{.items[0].metadata.name}")
+
+kubectl exec -it deploy/fastapi-deployment -- bash curl http://localhost

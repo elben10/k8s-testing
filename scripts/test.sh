d050df6741016b8aab9ba90b179eac8ba92823c3
Jakob Jul Elben <jelben@deloitte.dk>
2021-04-18 11:56:19 +0200

    Wait until deployment is ready

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,7 +6,6 @@ bash scripts/build.sh
 
 kubectl apply -f k8s/deployment.yml
 
-sleep 60
-#kubectl wait --for=condition=available --timeout=60s --all deployments
+kubectl wait --for=condition=available --timeout=60s --all deployments
 
 kubectl describe pod $(kubectl get pod -l app=fastapi -o jsonpath="{.items[0].metadata.name}")
